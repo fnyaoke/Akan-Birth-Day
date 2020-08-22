4b64a1f361b306c154cf99b5c314b36118229232
fnyaoke <fnyaoke@gmail.com>
2020-08-22 15:46:09 +0300

    find the index position of day of the week

--- js/script.js
+++ js/script.js
@@ -1,55 +1,27 @@
-var dateOfBirth = prompt('Please enter your date of birth');
+var dateOfBirth = prompt('Please enter your date of birth.');
 var birthMonth = prompt('Please enter the month you were born.');
-var yearOfBirth = prompt('Please enter the year you were born');
+var yearOfBirth = prompt('Please enter the year you were born.');
+var yourGender = prompt ('Please enter your gender assigned at birth.')
 alert('Your Birthday is ' + [dateOfBirth] + '/' + [birthMonth] + '/' + [yearOfBirth]);
-
+//convert the string input into number datatypes//
 var century = parseInt(yearOfBirth[0]+yearOfBirth[1]);
 var year = parseInt(yearOfBirth[2]+yearOfBirth[3]);
 var month = parseInt(birthMonth);
 var day = parseInt(dateOfBirth);
-alert('You were born in the '+ [year]+ 'th year of the '+[century]+'th century on the ' + [day] + 'th day of the ' + [month] + 'th month!' );
+alert('You were born in the '+ [year]+ 'th year of the '+[century]+'th century on the ' + [day] + 'th day of the ' + [month] + 'th month!' )
 
-var weekDay = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-function getDayOfWeek(){
-   var dayOfBirth = ( ( ([century]/4) -2*[century]-1) + ((5*[year]/4) ) + ((26*([month]+1)/10)) + [day] )%7;
-}
-return(dayOfBirth);
-alert('You were born on a ' + [weekDay])
-
-
-
-
-
-// var add = function(number1, number2) {
-//     return number1 + number2;
-//   };
-//   var subtract = function(number1, number2) {
-//   return number1 - number2;
-//   };
-//   alert(subtract(10,5));
-//   var add = function(number1, number2) {
-//     return number1 + number2;
-//   };
-//   var number1 = parseInt(prompt("Enter a number:"));
-//   var number2 = parseInt(prompt("Enter another number:"));
-//   alert(add(number1, number2));
-//   function calculateBmi() {
-//     var weight = document.bmiForm.weight.value
-//     var height = document.bmiForm.height.value
-//     if(weight > 0 && height > 0){	
-//     var finalBmi = weight/(height/100*height/100)
-//     document.bmiForm.bmi.value = finalBmi
-//     if(finalBmi < 18.5){
-//     document.bmiForm.meaning.value = "That you are too thin."
-//     }
-//     if(finalBmi > 18.5 && finalBmi < 25){
-//     document.bmiForm.meaning.value = "That you are healthy."
-//     }
-//     if(finalBmi > 25){
-//     document.bmiForm.meaning.value = "That you have overweight."
-//     }
-//     }
-//     else{
-//     alert("Please Fill in everything correctly")
-//     }
-//     }
+//calculating the day of the week//
+ function getDayOfWeek(d){
+    dayOfBirth = ( ( (([century]/4) -2)*([century]-1)) + ((5*[year]/4) ) + ((26*([month]+1)/10)) + [day] )%7;
+    return(dayOfBirth);
+ }
+ alert(getDayOfWeek());
+ var nDay = Math.trunc(dayOfBirth);
+ alert(nDay)
+ if (nDay>=3) {
+     nDayBorn= nDay -2;
+ } 
+ else {
+     nDayBorn=nDay;
+ };
+ 
