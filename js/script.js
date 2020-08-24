98678fd1f0225ad401fc248791bfe9b3f256e7e1
fnyaoke <fnyaoke@gmail.com>
2020-08-24 15:32:30 +0300

    figuring out the layout and linking to form

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-var dateOfBirth = prompt('Please enter your date of birth.');
-var birthMonth = prompt('Please enter the month you were born.');
-var yearOfBirth = prompt('Please enter the year you were born.');
-var yourGender = prompt ('Please enter your gender assigned at birth.')
+var dateOfBirth = document.form.days.value;
+var birthMonth = document.form.months.value;
+var yearOfBirth = document.form.year.value;
+var yourGender = document.form.gender-identity.value;
 alert('Your Birthday is ' + [dateOfBirth] + '/' + [birthMonth] + '/' + [yearOfBirth]);
 //convert the string input into number datatypes//
 var century = parseInt(yearOfBirth[0]+yearOfBirth[1]);
@@ -13,36 +13,63 @@ alert('You were born in the '+ [year]+ 'th year of the '+[century]+'th century o
 //calculating the day of the week//
  function getDayOfWeek(d){
     dayOfBirth = ( ( (([century]/4) -2)*([century]-1)) + ((5*[year]/4) ) + ((26*([month]+1)/10)) + [day] )%7;
-    return(dayOfBirth);
- }
- alert(getDayOfWeek());
- var nDay = Math.trunc(dayOfBirth);
- alert(nDay)
- if (nDay>=3) {
-     nDayBorn= nDay -2;
- }
- else if(nDay>=6){
-     nDayBorn= nDay -1;
- } 
- else{
-     nDayBorn=nDay;
- };
- //Display day of week
-var weekDay = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    var nDay = Math.trunc(dayOfBirth);
+        if (nDay>=3) {
+            nDayBorn= nDay -2;
+        }
+        else if(nDay>=6){
+            nDayBorn= nDay -1;
+        } 
+        else{
+            nDayBorn=nDay;
+        };
+        //Display day of week
+        var weekDay = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
-alert('You were born on a ' + (weekDay[nDayBorn]) + '!');
+        var dayOfWeek=weekDay[nDayBorn];
 
-//find Akan Name
-var maleAkanName = ["Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame", "Kwasi"];
-var femaleAkanName = ["Adwaa", "Abenaa", "Akua", "Yaa", "Afua", "Ama", "Akosua"];
-var gncAkanName = ["Adwaa-Kwadwo", "Abena-Kwabena", "Akua-Kwaku", "Yaa-Yaw", "Afua-Kifi","Ama-Kwame","Akosua-Kwasi" ]
+        //find Akan Name
+        var maleAkanName = ["Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame", "Kwasi"];
+        var femaleAkanName = ["Adwaa", "Abenaa", "Akua", "Yaa", "Afua", "Ama", "Akosua"];
+        var gncAkanName = ["Adwaa-Kwadwo", "Abena-Kwabena", "Akua-Kwaku", "Yaa-Yaw", "Afua-Kifi","Ama-Kwame","Akosua-Kwasi" ]
 
- if (yourGender==='Male') {
-     alert('Your Akan name is ' + (maleAkanName[nDayBorn]) + '!');
- } 
-    else if (yourGender==="Female") {
-    alert('Your Akan name is ' + (femaleAkanName[nDayBorn]) + '!');
+        if (yourGender==='Male') {
+            var akanName= maleAkanName[nDayBorn];
+        } 
+            else if (yourGender==="Female") {
+            var akanName= femaleAkanName[nDayBorn];
+        }
+        else{
+            var name = ncAkanName[nDayBorn];
+        };
  }
- else{
-    alert('Your Akan name is ' + (gncAkanName[nDayBorn]) + '!');
- };
+//  var nDay = Math.trunc(dayOfBirth);
+//  alert(nDay)
+//  if (nDay>=3) {
+//      nDayBorn= nDay -2;
+//  }
+//  else if(nDay>=6){
+//      nDayBorn= nDay -1;
+//  } 
+//  else{
+//      nDayBorn=nDay;
+//  };
+//  //Display day of week
+// var weekDay = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+// alert('You were born on a ' + (weekDay[nDayBorn]) + '!');
+
+// //find Akan Name
+// var maleAkanName = ["Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame", "Kwasi"];
+// var femaleAkanName = ["Adwaa", "Abenaa", "Akua", "Yaa", "Afua", "Ama", "Akosua"];
+// var gncAkanName = ["Adwaa-Kwadwo", "Abena-Kwabena", "Akua-Kwaku", "Yaa-Yaw", "Afua-Kifi","Ama-Kwame","Akosua-Kwasi" ]
+
+//  if (yourGender==='Male') {
+//      alert('Your Akan name is ' + (maleAkanName[nDayBorn]) + '!');
+//  } 
+//     else if (yourGender==="Female") {
+//     alert('Your Akan name is ' + (femaleAkanName[nDayBorn]) + '!');
+//  }
+//  else{
+//     alert('Your Akan name is ' + (gncAkanName[nDayBorn]) + '!');
+//  };
