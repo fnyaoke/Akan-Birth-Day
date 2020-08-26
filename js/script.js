660a3b6d12fd7dc4678b67585adf9c9dd2e37d27
fnyaoke <fnyaoke@gmail.com>
2020-08-26 10:23:20 +0300

    changes in script.js and index.html

--- js/script.js
+++ js/script.js
@@ -2,32 +2,35 @@ var dateOfBirth = document.getElementById("dateborn");
 var birthMonth = document.getElementById("birthmonth");
 var yearOfBirth = document.getElementById("year");
 var yourGender = document.getElementById("identity");
-alert('Your Birthday is ' + [dateOfBirth] + '/' + [birthMonth] + '/' + [yearOfBirth]);
+
 //convert the string input into number datatypes//
 var century = parseInt(yearOfBirth[0]+yearOfBirth[1]);
 var year = parseInt(yearOfBirth[2]+yearOfBirth[3]);
 var month = parseInt(birthMonth);
 var day = parseInt(dateOfBirth);
-alert('You were born in the '+ [year]+ 'th year of the '+[century]+'th century on the ' + [day] + 'th day of the ' + [month] + 'th month!' )
+// alert('You were born in the '+ [year]+ 'th year of the '+[century]+'th century on the ' + [day] + 'th day of the ' + [month] + 'th month!' )
+// var dayOfWeek = Date(dateOfBirth + birthMonth + yearOfBirth);
+// var weekDay= dayOfWeek.getDay();
+// alert(weekDay);
 
 //calculating the day of the week//
  function getDayOfWeek(d){
-    dayOfBirth = ( ( (([century]/4) -2)*([century]-1)) + ((5*[year]/4) ) + ((26*([month]+1)/10)) + [day] )%7;
-    var nDay = Math.trunc(dayOfBirth);
-        if (nDay>=3) {
-            nDayBorn= nDay -2;
-        }
-        else if(nDay>=6){
-            nDayBorn= nDay -1;
-        } 
-        else{
-            nDayBorn=nDay;
-        };
+     dayOfBirth = ( ( (([century]/4) -2)*([century]-1)) + ((5*[year]/4) ) + ((26*([month]+1)/10)) + [day] )%7;
+     var nDay = Math.trunc(dayOfBirth);
+         if (nDay>=3) {
+             nDayBorn= nDay -2;
+         }
+         else if(nDay>=6){
+             nDayBorn= nDay -1;
+         } 
+         else{
+             nDayBorn=nDay
+       };
  }
- //Display day of week
- var weekDay = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+ //Display day of week//
+  var weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
- var dayOfWeek =weekDay[nDayBorn];
+  var dayOfWeek =weekDay[nDayBorn];
 
  //find Akan Name
  var maleAkanName = ["Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame", "Kwasi"];
